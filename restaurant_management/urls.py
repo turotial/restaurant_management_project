1f98440da66ebe03c3c5646e70824980c1aa928e
Nagaraju <122417099+turotial@users.noreply.github.com>
2025-08-22 10:33:18 +0530

    22213

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('home.urls')),
+    path('',include('home.urls')),
     path('api/accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
